60d5942e5d5d01b1047662897efebec678e63b57
Adnan Sadik <adnansadik235@gmail.com>
2026-03-04 16:51:29 +0800

    added SQLITE for persistence

--- main.py
+++ main.py
@@ -1,39 +1,81 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+##database setup
+SQLALCHEMY_DATABASE_URL ="sqlite:///./tasks.db"
+engine = create_engine (SQLALCHEMY_DATABASE_URL, connect_args ={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
+Base = declarative_base()
+##task model
+class TaskDB(Base):
+    __tablename__ = "tasks"
+    id=Column(Integer,primary_key=True, index=True)
+    name = Column(String,index=True)
+    deadline= Column(String)
+
+##create tables
+Base.metadata.create_all(bind=engine)
 app =FastAPI()
-tasks=[]
+
 from pydantic import BaseModel
-class Task(BaseModel):
-    name: str
-    deadline: str
-    id: int
+
 @app.post('/tasks/')
 
 def add_task( task: Task):
-    tasks.append(task)
-    return {"name": task.name, "deadline": task.deadline, "id":task.id}
+    db=SessionLocal()
+    db_task = TaskDB(name= task.name,deadline= task.deadline)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task) ## this loads the new id and data from the db
+    db.close()
+    return {"name": db_task.name, "deadline": db_task.deadline, "id":db_task.id}
+
 @app.get('/tasks/')
 def list_tasks():
-    return tasks
+    db= SessionLocal()
+    tasks= db.query(TaskDB).all()
+    dict_list=[]
+    for task in tasks:
+        dict_list.append({"name": task.name, "deadline": task.deadline, "id":task.id})
+    db.close()
+    return dict_list
 @app.get('/tasks/{id}')
 def get_task(id: int):
-    for task in tasks:
-        if task.id == id:
-            return task
-    return {"task not found"}
+
+    db= SessionLocal()
+    task= db.query(TaskDB).get(id)
+    db.close()
+    if task !=None :
+        return {"id": task.id, "name": task.name, "deadline": task.deadline}
+    else:
+        return {"task not found"}
 
 @app.put('/tasks/{id}')
 def update_task( id:int,modified_task: Task):
-    for i in range(len(tasks)):
-        if id== tasks[i].id:
-            tasks[i]=modified_task
-            return modified_task
-    return {"message" : "task not found"}
+    db=SessionLocal()
+    task= db.query(TaskDB).get(id)
+    if task!= None:
+        task.name, task.deadline = modified_task.name, modified_task.deadline
+        db.commit()
+        db.close()
+        return {"id": task.id, "name": task.name, "deadline": task.deadline}
+    else:
+        db.close()
+        return {"message" : "task not found"}
 @app.delete('/tasks/{id}')
 def delete_task (id: int):
-    for task in tasks:
-        if task.id==id:
-            tasks.remove(task)
-            return {"message": "successfully deleted task"}
-    return {"message" : "failed to delete task"}
+    db=SessionLocal()
+    task= db.query(TaskDB).get(id)
+    if task!= None:
+        
+        db.delete(task)
+        db.commit()
+        db.close()
+        return {"id": task.id, "name": task.name, "deadline": task.deadline}
+    else:
+        db.close()
+        return {"message" : "task not found"}
+
 
 
